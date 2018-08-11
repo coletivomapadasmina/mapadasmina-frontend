79989c231097f8643e75eaa19dbc39f4d2de106e
Paula Grangeiro <contato@paulagrangeiro.com.br>
2018-08-11 19:58:21 -0300

    Minor fixes

--- src/Componentes/Header/Header.js
+++ src/Componentes/Header/Header.js
@@ -3,7 +3,17 @@ import './Header.css';
 
 function Header(props) {
     return (
-      <div className="position-relative overflow-hidden p-3 p-md-5 m-md-3 text-center bg-light">Oi</div>
+      <div className="container header">
+        <div className="row">
+            <div className="col-md-7">
+                <iframe width="560" height="315" src="https://www.youtube.com/embed/t7lM7Bn16Zg" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+            </div>
+            <div className="col-md-5 text">
+              <p>Texto de apresentação #mapas da mina</p>
+              <a href="#"><button className="btn btn-mais">Saiba Mais</button></a>
+            </div>
+        </div>
+      </div>
     )
 }
 
