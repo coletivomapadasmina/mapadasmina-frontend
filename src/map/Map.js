729f61cce0a0d71ec83e418c23c45294b4843f8e
Darlene <darlenedms3@gmail.com>
2018-08-11 16:15:54 -0300

    Move map components to a specific folder

--- src/map/Map.js
+++ src/map/Map.js
@@ -31,32 +31,4 @@ const MyMapComponent = compose(
   </GoogleMap>
 ));
 
-export class Map extends React.PureComponent {
-  state = {
-    isMarkerShown: false
-  };
-
-  componentDidMount() {
-    this.delayedShowMarker();
-  }
-
-  delayedShowMarker = () => {
-    setTimeout(() => {
-      this.setState({ isMarkerShown: true });
-    }, 3000);
-  };
-
-  handleMarkerClick = () => {
-    this.setState({ isMarkerShown: false });
-    this.delayedShowMarker();
-  };
-
-  render() {
-    return (
-      <MyMapComponent
-        isMarkerShown={this.state.isMarkerShown}
-        onMarkerClick={this.handleMarkerClick}
-      />
-    );
-  }
-}
+export default MyMapComponent
---
+++ src/map/MapWrapper.js
@@ -0,0 +1,32 @@
+import React from "react";
+import Map from './Map'
+
+export default class MapWrapper extends React.PureComponent {
+  state = {
+    isMarkerShown: false
+  };
+
+  componentDidMount() {
+    this.delayedShowMarker();
+  }
+
+  delayedShowMarker = () => {
+    setTimeout(() => {
+      this.setState({ isMarkerShown: true });
+    }, 3000);
+  };
+
+  handleMarkerClick = () => {
+    this.setState({ isMarkerShown: false });
+    this.delayedShowMarker();
+  };
+
+  render() {
+    return (
+      <Map
+        isMarkerShown={this.state.isMarkerShown}
+        onMarkerClick={this.handleMarkerClick}
+      />
+    );
+  }
+}
