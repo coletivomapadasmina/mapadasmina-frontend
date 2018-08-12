a160d446e82a18583580e97cc2172649fac8db7b
Paula Grangeiro <contato@paulagrangeiro.com.br>
2018-08-12 12:11:44 -0300

    Fix responsive video

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -6,8 +6,13 @@ function Header(props) {
       <section className="header">
           <div className="container">
               <div className="row">
-                  <div className="col-md-7">
-                      <iframe width="560" height="315" src="https://www.youtube.com/embed/t7lM7Bn16Zg" frameBorder="0" allow="autoplay; encrypted-media" allowFullScreen></iframe>
+                  <div className="col-md-7 embed-responsive  embed-responsive-16by9">
+                      <iframe
+                        className="embed-responsive-item"
+                        src="https://www.youtube.com/embed/t7lM7Bn16Zg"
+                        frameBorder="0"
+                        allow="autoplay; encrypted-media"
+                        allowFullScreen />
                   </div>
                   <div className="col-md-5 text">
                       <p>Texto de apresentação #mapas da mina</p>
