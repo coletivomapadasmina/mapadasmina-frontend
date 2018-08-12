ba286e12ac4a9281185768909cc34a4dfbcbbbaf
Paula Grangeiro <contato@paulagrangeiro.com.br>
2018-08-11 22:50:00 -0300

    Fix minor errors

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -7,7 +7,7 @@ function Header(props) {
           <div className="container">
               <div className="row">
                   <div className="col-md-7">
-                      <iframe width="560" height="315" src="https://www.youtube.com/embed/t7lM7Bn16Zg" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+                      <iframe width="560" height="315" src="https://www.youtube.com/embed/t7lM7Bn16Zg" frameBorder="0" allow="autoplay; encrypted-media" allowFullScreen></iframe>
                   </div>
                   <div className="col-md-5 text">
                       <p>Texto de apresentação #mapas da mina</p>
