523aafd21f825943fbd2201498150edf2aa95fe0
Darlene <darlenedms3@gmail.com>
2018-08-15 00:36:35 -0300

    Make video fill the screen

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -4,23 +4,25 @@ import './Header.css';
 function Header(props) {
     return (
       <section className="header d-flex align-items-center h-100">
-          <div className="container">
-              <div className="row">
-                  <div className="col-md-7 embed-responsive  embed-responsive-16by9">
-                      <iframe
-                        title="Mapa das Mina"
-                        className="embed-responsive-item"
-                        src="https://www.youtube.com/embed/A1Ne7iXNtDo"
-                        frameBorder="0"
-                        allow="autoplay; encrypted-media"
-                        allowFullScreen />
-                  </div>
-                  <div className="col-md-5 text">
-                      <p>Texto de apresentação #mapas da mina</p>
-                      <a href="#"><button className="btn btn-mais">Saiba Mais</button></a>
-                  </div>
-              </div>
+          <div className="video-container">
+            <div className="embed-responsive embed-responsive-16by9">
+                <iframe
+                  title="Mapa das Mina"
+                  className="embed-responsive-item"
+                  src="https://www.youtube.com/embed/A1Ne7iXNtDo"
+                  frameBorder="0"
+                  allow="autoplay; encrypted-media"
+                  allowFullScreen />
+            </div>
           </div>
+        <div className="go-to-map">
+            <a className="link-to-map" href="#mapa">
+              <span>IR PARA O MAPA</span>
+              <div className="circle-to-map">
+                <i className="down" aria-hidden="true"></i>
+              </div>
+            </a>
+        </div>
       </section>
     )
 }
