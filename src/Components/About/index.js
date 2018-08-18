4bc6815c8213e938bcee2820fc70c377a498adb0
Darlene <darlenedms3@gmail.com>
2018-08-18 20:38:23 -0300

    Add background images and styles to Motivation and About sections

--- src/Components/About/index.js
+++ src/Components/About/index.js
@@ -1,14 +1,19 @@
 import React from 'react'
+import './styles.css'
 
 const About = () => (
   <section id="about">
-    <h3>Quem somos</h3>
-    <p>O #mapadasmina é um coletivo criado em agosto de 2017 com o objetivo promover, visibilizar e apoiar as candidaturas de mulheres nas eleições 2018, inicialmente no estado do Rio de Janeiro
+    <div className="content">
+      <h3>Quem somos</h3>
+      <p>O #mapadasmina é um coletivo criado em agosto de 2017 com o objetivo promover, visibilizar e apoiar as candidaturas de mulheres nas eleições 2018, inicialmente no estado do Rio de Janeiro.</p>
 
-    O que acreditamos
-    O #mapadasmina acredita que a igualdade de gênero nos espaços de tomada de decisão é fundamental para o fortalecimento da democracia brasileira.
-    Porque a gente quer a paridade e quer agora! Cola com a gente que juntas somos maioria.
-    </p>
+      <h5>O que acreditamos</h5>
+      <p>O #mapadasmina acredita que a igualdade de gênero nos espaços de tomada de decisão é fundamental para o fortalecimento da democracia brasileira.</p>
+      <p>Porque a gente quer a paridade e quer agora!</p>
+      <p>Cola com a gente que juntas somos maioria.</p>
+
+      <h4>Encontre a gente</h4>
+    </div>
   </section>
 )
 
--- src/Components/Motivation/index.js
+++ src/Components/Motivation/index.js
@@ -1,17 +1,21 @@
 import React from 'react'
+import './styles.css'
 
 const Motivation = () => (
   <section id="motivation">
-    <h3>Por que votar em mulheres?</h3>
-    <p>OA ausência ou disparidade no número de mulheres ocupando cargos políticos no Brasil é visível através de qualquer imagem que retrate esses espaços, mas para além da suposição, dados da publicação "Women in Politics: 2017", da ONU Mulheres, aponta o Brasil na 154ª posição no ranking internacional de paridade de gênero em cargos políticos executivos. O constante descaso no que diz respeito à falta de políticas sensíveis às questões de gênero que considerem, de fato, o bem-estar e plenos direitos das mulheres também denuncia a falta de representatividade feminina neste ambiente de tomada de decisões.
-    O #mapadasmina configura o ponto de encontro de indivíduos que compartilham de uma afinidade em comum: a busca por esta paridade, proporcionando a partir desta ideia a possibilidade de articulação dessa rede de pessoas e seus saberes com o objetivo de mudar a atual conjuntura política brasileira.</p>
+    <div className="content">
+      <h3>Por que votar em mulheres?</h3>
+      <p>A ausência ou disparidade no número de mulheres ocupando cargos políticos no Brasil é visível através de qualquer imagem que retrate esses espaços, mas para além da suposição, dados da publicação "Women in Politics: 2017", da ONU Mulheres, aponta o Brasil na 154ª posição no ranking internacional de paridade de gênero em cargos políticos executivos.</p>
+      <p>O constante descaso no que diz respeito à falta de políticas sensíveis às questões de gênero que considerem, de fato, o bem-estar e plenos direitos das mulheres também denuncia a falta de representatividade feminina neste ambiente de tomada de decisões.</p>
+      <p>O #mapadasmina configura o ponto de encontro de indivíduos que compartilham de uma afinidade em comum: a busca por esta paridade, proporcionando a partir desta ideia a possibilidade de articulação dessa rede de pessoas e seus saberes com o objetivo de mudar a atual conjuntura política brasileira.</p>
 
-    <h5>Contexto político, social e econômico</h5>
+      <h5>Contexto político, social e econômico</h5>
 
-    <p>Em todo o mundo e, especialmente, no Brasil, vive-se uma realidade política de sub-representação das mulheres em cargos eletivos. Um levantamento do Tribunal Superior Eleitoral (TSE), baseado nos dados do sistema DivulgaCandContas​, aponta que, no ano de 2016, 31,6% dos candidatos a prefeito e vereador eram do sexo feminino, e 68,4% do sexo masculino. Do total de candidatas, apenas 13% foram eleitas.
-    Nas eleições de 2014, 31% dos candidatos gerais eram mulheres. Contudo, apenas 11% foram eleitas. As mulheres ainda detêm as candidaturas de 10% dos deputados federais e 14% dos senadores,  com percentual idêntico nas Assembleias Estaduais, e menor ainda nas Câmaras de Vereadores e no Poder Executivo.
-    Diante dos quadros alarmantes, percebe-se a dificuldade dos partidos e coligações de atenderem o disposto na Lei das Eleições ​(​Lei nº 9.504/1997​), que estabelece a obrigatoriedade de um percentual mínimo de mulheres nas disputas eleitorais (após a reforma eleitoral de 2009 através da Lei 12.034/2009). O TSE já consolidou jurisprudência de que esse preenchimento é obrigatório, entendendo q​ue, "na impossibilidade de registro de candidaturas femininas no percentual mínimo de 30%, o partido ou a coligação deve reduzir​ ​o​ ​número​ ​de​ ​candidatos​ ​do​ ​sexo​ ​masculino​ ​para​ ​se​ ​adequar​ ​às​ ​cotas​ ​de​ ​gênero".​
-    </p>
+      <p>Em todo o mundo e, especialmente, no Brasil, vive-se uma realidade política de sub-representação das mulheres em cargos eletivos. Um levantamento do Tribunal Superior Eleitoral (TSE), baseado nos dados do sistema DivulgaCandContas​, aponta que, no ano de 2016, 31,6% dos candidatos a prefeito e vereador eram do sexo feminino, e 68,4% do sexo masculino. Do total de candidatas, apenas 13% foram eleitas.</p>
+      <p>Nas eleições de 2014, 31% dos candidatos gerais eram mulheres. Contudo, apenas 11% foram eleitas. As mulheres ainda detêm as candidaturas de 10% dos deputados federais e 14% dos senadores,  com percentual idêntico nas Assembleias Estaduais, e menor ainda nas Câmaras de Vereadores e no Poder Executivo.</p>
+      <p>Diante dos quadros alarmantes, percebe-se a dificuldade dos partidos e coligações de atenderem o disposto na Lei das Eleições (Lei nº 9.504/1997), que estabelece a obrigatoriedade de um percentual mínimo de mulheres nas disputas eleitorais (após a reforma eleitoral de 2009 através da Lei 12.034/2009). O TSE já consolidou jurisprudência de que esse preenchimento é obrigatório, entendendo q​ue, "na impossibilidade de registro de candidaturas femininas no percentual mínimo de 30%, o partido ou a coligação deve reduzir o número ​de​ ​candidatos​ ​do​ ​sexo​ ​masculino​ ​para​ ​se​ ​adequar​ ​às​ ​cotas​ ​de​ ​gênero".​
+      </p>
+    </div>
   </section>
 )
 
