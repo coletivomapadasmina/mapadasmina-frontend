99471a1b11d11ae74d554e58bbb6f94e7afe3d3e
Darlene <darlenedms3@gmail.com>
2018-08-21 21:00:12 -0300

    Change pin colors and consume Google's pin image

--- src/utils.js
+++ src/utils.js
@@ -1,13 +1,16 @@
-import senator from './assets/img/pin_senator.png'
-import federalDeputy from './assets/img/pin_federal_deputy.png'
-import stateDeputy from './assets/img/pin_state_deputy.png'
+const pinBaseUrl = 'https://www.google.com/maps/vt/icon/name=assets/icons/spotlight/spotlight_pin_v2_shadow-1-small.png,assets/icons/spotlight/spotlight_pin_v2-1-small.png,assets/icons/spotlight/spotlight_pin_v2_dot-1-small.png,assets/icons/spotlight/spotlight_pin_v2_accent-1-small.png'
 
 export function getIcon(role) {
   const roles = {
-    1: senator,
-    2: federalDeputy,
-    3: stateDeputy
+    1: { name: 'Vereadora', primaryColor: '345995', secondaryColor: '1a2c4a' },
+    2: { name: 'Senadora', primaryColor: 'EAC435', secondaryColor: '8c751f' },
+    3: { name: 'Governadora', primaryColor: '4F345A', secondaryColor: '271a2d' },
+    4: { name: 'Deputada Federal', primaryColor: '36C9C6', secondaryColor: '1b6463' },
+    5: { name: 'Deputada Estadual', primaryColor: 'DF367C', secondaryColor: '6f1b3e' },
+    6: { name: 'Presidenta', primaryColor: 'ea4335', secondaryColor: '960a0a' },
+    7: { name: 'Nunca se candidatou', primaryColor: '6610F2', secondaryColor: '330879' }
   }
 
-  return roles[role] || undefined
+  const currentRole = roles[role]
+  return `${pinBaseUrl}&highlight=ff000000,${currentRole.primaryColor},${currentRole.secondaryColor},ff000000&color=ff000000?scale=1`
 }
