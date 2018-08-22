08febc35c3114a3f40f6e70bebb60c114b920c9b
Darlene <darlenedms3@gmail.com>
2018-08-21 22:17:17 -0300

    Style causes items

--- src/Components/Causes/CausesItem.js
+++ src/Components/Causes/CausesItem.js
@@ -2,13 +2,13 @@ import React from 'react'
 
 const CausesItems = (props) => (
   <li className="causes-item">
-    <label>
+    <label className="causes-label">{props.cause.title}
       <input
         id={props.cause.id}
         type="checkbox"
         onChange={props.onChange}
       />
-      {props.cause.title}
+      <span className="checkmark"></span>
     </label>
   </li>
 )
