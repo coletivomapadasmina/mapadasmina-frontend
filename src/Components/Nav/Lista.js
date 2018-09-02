bd5184a749fc5cc90c826fcf72b0f2e451e422f3
Darlene <darlenedms3@gmail.com>
2018-09-02 12:15:15 -0300

    Adjust indentation of nav bar links file

--- src/Components/Nav/Lista.js
+++ src/Components/Nav/Lista.js
@@ -2,23 +2,22 @@ import React from 'react';
 import './Nav.css';
 
 function Lista(props) {
-
-    return (
-        <ul className="links">
-            <li>
-                <a href="#mapa">o mapa</a>
-            </li>
-            <li>
-                <a href="#motivation">por que votar em mulheres?</a>
-            </li>
-            <li>
-                <a href="#about">quem somos</a>
-            </li>
-            <li className="d-none">
-                <a href="#"><button className="btn btn-doe">doe para o #mapa</button></a>
-            </li>
-        </ul >
-    )
+  return (
+    <ul className="links">
+      <li>
+        <a href="#mapa">o mapa</a>
+      </li>
+      <li>
+        <a href="#motivation">por que votar em mulheres?</a>
+      </li>
+      <li>
+        <a href="#about">quem somos</a>
+      </li>
+      <li className="d-none">
+        <a href="#"><button className="btn btn-doe">doe para o #mapa</button></a>
+      </li>
+    </ul>
+  )
 }
 
 export default Lista
